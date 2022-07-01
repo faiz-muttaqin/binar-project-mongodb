45301012760c1713ef4b5b221ea43ff165f21aab
Faiz Muttaqin <muttaqinfaiz@gmail.com>
2022-07-01 16:26:50 +0700

    input fitur login & login restrction

--- routes/routes.js
+++ routes/routes.js
@@ -19,13 +19,6 @@ Routes.get("/login", (req, res) => {
 });
 
 Routes.post("/login", (req, res) => {
-  // let dataUser = [
-  //   { user: "faizmuttaqin", pass: "34160049" },
-  //   { user: "naruto", pass: "02193983" },
-  //   { user: "sasuke", pass: "09139218" },
-  //   { user: "sakura", pass: "48200239" },
-  //   { user: "sasori", pass: "01398343" },
-  // ];
   let requestData = req.body;
   let newUser = requestData.username;
   let newPass = requestData.password;
--- user_log/user_log.js
+++ user_log/user_log.js
@@ -1,11 +1,41 @@
 //database
 
 let dataUser = [
-  { user: "faizmuttaqin", pass: "34160049" },
-  { user: "naruto", pass: "02193983" },
-  { user: "sasuke", pass: "09139218" },
-  { user: "sakura", pass: "48200239" },
-  { user: "sasori", pass: "01398343" },
+  {
+    id: 1,
+    user: "faizmuttaqin",
+    email: "muttaqinfaiz@gmail.com",
+    pass: "34160049",
+    token: "92rhj3rh289-j23-",
+  },
+  {
+    id: 2,
+    user: "naruto",
+    email: "naruto@gmail.com",
+    pass: "12345",
+    token: "920j1-j9-1ejd0ws9i",
+  },
+  {
+    id: 3,
+    user: "sasuke",
+    email: "sasuke@gmail.com",
+    pass: "12345",
+    token: "012-j-0qwedw-9i2ejdeqpnfd02",
+  },
+  {
+    id: 4,
+    user: "rocklee",
+    email: "rocklee@gmail.com",
+    pass: "12345",
+    token: "-320rj-39qiew322i0rm32320wji",
+  },
+  {
+    id: 5,
+    user: "kakashi",
+    email: "kakashi@gmail.com",
+    pass: "12345",
+    token: "21IRj-92qe9d9023nf-pw2",
+  },
 ];
 
 module.exports = dataUser;
